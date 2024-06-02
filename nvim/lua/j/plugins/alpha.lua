4c8e6ddf2b9af0629b5973ad7554797b19812550
James Scholz <45739456+jvscholz@users.noreply.github.com>
2024-06-02 01:57:28 -0600

    Delete nvim directory

--- nvim/lua/j/plugins/alpha.lua
+++
@@ -1,6 +0,0 @@
-return {
-    'goolord/alpha-nvim',
-    config = function ()
-        require'alpha'.setup(require'alpha.themes.dashboard'.config)
-    end,
-}
--- nvim/lua/j/plugins/gruvbox.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'f4z3r/gruvbox-material.nvim',
-  name = 'gruvbox-material',
-  lazy = false,
-  priority = 1000,
-  opts = {},
-}
-	
-
