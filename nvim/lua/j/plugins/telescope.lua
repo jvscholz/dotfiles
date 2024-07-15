c3cbc16941400b3d26aa899f94338814576f5ca2
j <jvscholz@gmail.com>
2024-07-15 12:39:36 -0700

    adds telescope

--- nvim/lua/j/plugins/telescope.lua
+++ nvim/lua/j/plugins/telescope.lua
@@ -1,4 +1,5 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+-- or                              , branch = '0.1.x',
       dependencies = { 'nvim-lua/plenary.nvim' }
-}
+    }
