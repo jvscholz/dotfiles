fc916e7048c24f30f3e75db260e8710b1f82cb9c
j <jvscholz@gmail.com>
2024-08-03 14:59:03 -0700

    adds japanese and more roam templates

--- doom/config.el
+++ doom/config.el
@@ -50,15 +50,39 @@
 ;;   :config
 ;;   (global-pangu-spacing-mode 1))
 
-(setq org-roam-capture-templates
-      '(("d" "default" plain "%?"
-         :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
-                            "#+title: ${title}\n")
-         :unnarrowed t)))
+;; (cl-defmethod org-roam-node-type ((node org-roam-node))
+;;   "Return the TYPE of NODE."
+;;   (condition-case nil
+;;       (file-name-nondirectory
+;;        (directory-file-name
+;;         (file-name-directory
+;;          (file-relative-name (org-roam-node-file node) org-roam-directory))))
+;;     (error "")))
+;;
+(setq org-roam-node-display-template
+      (concat "${type:15} ${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
 
 (setq org-roam-capture-templates
-      '(("g" "Genki Note" plain
-         "%?"
+      '(("m" "main" plain "%?"
+         :if-new (file+head "main/${slug}.org"
+                            "#+title: ${title}\n")
+         :immediate-finish t
+         :unnarrowed t)
+        ("r" "reference" plain "%?"
+         :if-new (file+head "reference/${title}.org"
+                            "#+title: ${title}\n")
+         :immediate-finish t
+         :unnarrowed t)
+        ("a" "article" plain "%?"
+         :if-new (file+head "articles/${title}.org"
+                            "#+title: ${title}\n#+filetags: :article:\n")
+         :immediate-finish t
+         :unnarrowed t)
+        ("d" "default" plain "%?"
+         :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
+                            "#+title: ${title}\n")
+         :unnarrowed t)
+        ("g" "Genki Note" plain "%?"
          :if-new (file+head "reference/Genki/${slug}.org"
                             "#+title: ${title}\n#+date: %U\n")
          :unnarrowed t)))
@@ -93,6 +117,14 @@
 (use-package vterm
   :ensure t
   :commands vterm)
+
+(setq default-input-method "japanese")
+
+(general-define-key
+ :states 'insert
+ "q" (general-key-dispatch 'self-insert-command
+       :timeout 0.25
+       "q" 'toggle-input-method))
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
