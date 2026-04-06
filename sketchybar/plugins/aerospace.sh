09ce24b947c52f5e5ff42a0ef9a56c210b024a89
James Scholz <jvscholz@Jamess-MacBook.local>
2026-04-06 15:20:46 -0700

    add icons

--- sketchybar/plugins/aerospace.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME background.drawing=on
-else
-    sketchybar --set $NAME background.drawing=off
-fi
--- sketchybar/plugins/clock.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Format: Month/Day/Year Hour:Minute AM/PM
-DATETIME=$(date '+%m/%d/%Y %I:%M %p')
-sketchybar --set $NAME label="$DATETIME"
